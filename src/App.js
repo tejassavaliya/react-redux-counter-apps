094d421860404e83af241ce647ea77f3a9932eb4
Tejas.S <tejas.s@ezdi.us>
2020-07-30 14:44:49 +0530

    setup redux for counter increment and decrement

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from './actions'
 
 function App() {
+  const counter = useSelector(state => state.counter);
+  const isLogged = useSelector( state => state.isLogged );
+  const dispatch = useDispatch();
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>Counter : {counter} </h1>
+      { isLogged ? <h3> Valuable Information I shouldn't see</h3> : '' }
+      
+      <button onClick={() => dispatch(increment())}>+</button>
+      <span>&nbsp;</span>
+      <button onClick={() => dispatch(decrement())}>-</button>
     </div>
   );
 }
