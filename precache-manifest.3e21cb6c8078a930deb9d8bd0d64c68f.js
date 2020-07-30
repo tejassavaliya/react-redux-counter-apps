9e4b208136cbb01f99f484f42929fdef7adc8ef6
Tejas.S <tejas.s@ezdi.us>
2020-07-30 14:54:58 +0530

    Updates

---
+++ precache-manifest.3e21cb6c8078a930deb9d8bd0d64c68f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c9182455d16a222b68f8ce1e7b0b967",
+    "url": "/react-redux-counter-apps/index.html"
+  },
+  {
+    "revision": "6135c37133b1a8cf935b",
+    "url": "/react-redux-counter-apps/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0f32c82cb80b02de1ca3",
+    "url": "/react-redux-counter-apps/static/js/2.bb85c4f3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-redux-counter-apps/static/js/2.bb85c4f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6135c37133b1a8cf935b",
+    "url": "/react-redux-counter-apps/static/js/main.ac61f8d7.chunk.js"
+  },
+  {
+    "revision": "ec6872427ceb0f603451",
+    "url": "/react-redux-counter-apps/static/js/runtime-main.3184a883.js"
+  }
+]);
